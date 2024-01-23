8f818a86868ff981b51fe2c6ca99c270a645b53a
Solomon Gachure <gachuresolomon64@gmail.com>
2024-01-23 14:38:41 +0300

    Changes to the contacts page

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,8 +1,38 @@
 import React from 'react'
-
+import {FaFacebook, FaInstagram, FaTiktok, FaWhatsapp} from 'react-icons/fa'
 const Contacts = () => {
+  const WhatsAppLink = ()=>{
+    const number='+254798605270'
+    const message="welcome to dee makeup services!"
+    const whatsappUrl = `https://api.whatsapp.com/send?phone=${number}&text=${encodeURIComponent(message)}`;
+    window.location.href = whatsappUrl;
+  }
   return (
-    <div>Contacts</div>
+    <div className='h-full'>
+      <div>
+        <h1 className='text-center text-4xl md:text-5xl p-1 text-cyan-700'>Reach us through:</h1>
+        <div>
+          <form action="https://getform.io/f/db96c5fb-9063-4883-a850-e833759e93be" className='flex flex-col items-center h-full w-full  p-4 gap-2 text-black' method="post">
+            <div className='justify-between flex gap-2'>
+              <input className=' p-2 w-[180px] md:w-full outline-none bg-transparent  border rounded-sm' required type="text" name='name' placeholder='Name' />
+              <input className=' p-2 w-[180px] md:w-full outline-none bg-transparent  border rounded-sm' required type="text" name='email'  placeholder='Email' />
+            </div>
+            <textarea className='md:w-[500px] w-[350px] p-2 outline-none bg-transparent border rounded-sm' required name="message" placeholder='Message' id="" cols="30" rows="10"></textarea>
+            <button className='text-white bg-cyan-500 p-2 w-[200px] font-bold rounded-sm text-xl'>Submit</button>
+          </form>
+        </div>
+        <div>
+          <h1 className='text-center text-gray-500'>Also follow us through our socials</h1>
+          <div className='flex justify-center items-center gap-14 p-2'>
+            {/**icons for the socials */}
+            <FaFacebook size={30} className='hover:scale-105 duration-300 cursor-pointer' />
+            <FaInstagram size={30} className='hover:scale-105 duration-300 cursor-pointer' />
+            <FaTiktok size={30} className='hover:scale-105 duration-300 cursor-pointer' />
+              <FaWhatsapp size={30} target='_blank' onClick={WhatsAppLink} className='hover:scale-105 duration-300 cursor-pointer' />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/exercises.js
+++ src/components/exercises.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import jsonData from '../data/data.json'
 
-const Exercises = () => {
-  return (
-    <div>Exercises</div>
-  )
-}
+  const Exercises = () => {
+    const [data, setData]=useState([])
+
+    useEffect(()=>{
+      fetchData();
+    }, [])
+    const fetchData= async ()=>{
+      try{
+        setData(jsonData)
+      }
+      catch(error){
+        console.log('error', error)
+      }
+    }
+    const handleButtonClick = async () => {
+      fetchData();
+    };
+    return (
+      <div>
+        {data.map((item) =>(
+          <div key={item.id}>
+          {/**section to show the type and name of the workouts */}
+          <div>
+  <h1>Exercise type : {item.title}</h1>
+  <h1>Duration : {item.duration} </h1>
+  <h1>Description : {item.description} </h1>
+          </div>
+          <button className='bg-green-500' onClick={handleButtonClick}>Fetch New Data</button>
+          </div>
+        ))}
+      </div>
+    )
+  }
 
 export default Exercises
